89edd3ba1374d157ee87e65cae0f61a99289a4ee
MateoDedi <mateod@live.com>
2023-10-19 11:27:39 +0200

    pls work

--- script.js
+++ script.js
@@ -3,25 +3,25 @@ const form = document.querySelector("form");
 const grid = document.querySelector(".grid");
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  fetchData();
+    event.preventDefault();
+    fetchData();
 });
 
 async function fetchData() {
-  grid.innerHTML = "";
-  const inputValue = input.value;
-  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`);
-  const data = await response.json();
-  displayResults(data.meals);
+    grid.innerHTML = "";
+    const inputValue = input.value;
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`);
+    const data = await response.json();
+    displayResults(data.meals);
 }
 
 function displayResults(meals) {
-  if (!meals) {
-    grid.innerHTML = '<p>No results found.</p>';
-    return;
-  }
+    if (!meals) {
+        grid.innerHTML = '<p>No results found.</p>';
+        return;
+    }
 
-  grid.innerHTML = meals.map((meal) => `
+    grid.innerHTML = meals.map((meal) => `
     <div class="meal">
       <div class="text">
         <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
@@ -33,16 +33,16 @@ function displayResults(meals) {
     </div>
   `).join("");
 
-  grid.querySelectorAll(".meal").forEach((meal) => {
-    meal.addEventListener("click", () => {
-      meal.querySelector(".modalcontainer").classList.toggle("hidden");
+    grid.querySelectorAll(".meal").forEach((meal) => {
+        meal.addEventListener("click", () => {
+            meal.querySelector(".modalcontainer").classList.toggle("hidden");
+        });
     });
-  });
 }
 
 grid.addEventListener("click", (event) => {
-  const modalContainer = event.target.closest(".modalcontainer");
-  if (modalContainer) {
-    modalContainer.classList.add("hidden");
-  }
+    const modalContainer = event.target.closest(".modalcontainer");
+    if (modalContainer) {
+        modalContainer.classList.add("hidden");
+    }
 });
